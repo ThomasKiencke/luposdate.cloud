e05f167940b76d2bc357c432f41449601c7b5ce2
ThomasKiencke <thomas.kiencke@gmail.com>
2013-08-02 11:54:50 +0200

    .

--- src/main/java/lupos/cloud/pig/udfs/PigLoadUDF.java
+++ src/main/java/lupos/cloud/pig/udfs/PigLoadUDF.java
@@ -2,8 +2,15 @@ package lupos.cloud.pig.udfs;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
 
+import org.apache.commons.cli.ParseException;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.Job;
@@ -14,202 +21,143 @@ import org.apache.pig.LoadFunc;
 import org.apache.pig.PigException;
 import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
+import org.apache.pig.backend.hadoop.hbase.HBaseStorage;
+import org.apache.pig.backend.hadoop.hbase.HBaseStorage.ColumnInfo;
+import org.apache.pig.data.BinSedesTuple;
 import org.apache.pig.data.DataByteArray;
+import org.apache.pig.data.DefaultTuple;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 
-public class PigLoadUDF extends LoadFunc {
+public class PigLoadUDF extends HBaseStorage {
+	private RecordReader reader;
 
-	 protected RecordReader in = null;
-	    private byte fieldDel = '\t';
-	    private ArrayList<Object> mProtoTuple = null;
-	    private TupleFactory mTupleFactory = TupleFactory.getInstance();
-	    private static final int BUFFER_SIZE = 1024;
+	@Override
+	public void prepareToRead(RecordReader reader, PigSplit split) {
+		super.prepareToRead(reader, split);
+		this.reader = reader;
+	}
 
-	    public PigLoadUDF() {
-	    }
+	public PigLoadUDF(String columnList, String optString)
+			throws ParseException, IOException {
+		super(columnList, optString);
+	}
 
-	    /**
-	     * Constructs a Pig loader that uses specified character as a field delimiter.
-	     *
-	     * @param delimiter
-	     *            the single byte character that is used to separate fields.
-	     *            ("\t" is the default.)
-	     */
-	    public PigLoadUDF(String delimiter) {
-	        this();
-	        if (delimiter.length() == 1) {
-	            this.fieldDel = (byte)delimiter.charAt(0);
-	        } else if (delimiter.length() >  1 && delimiter.charAt(0) == '\\') {
-	            switch (delimiter.charAt(1)) {
-	            case 't':
-	                this.fieldDel = (byte)'\t';
-	                break;
+	public PigLoadUDF(String columnList) throws ParseException, IOException {
+		this(columnList, "");
+	}
 
-	            case 'x':
-	               fieldDel =
-	                    Integer.valueOf(delimiter.substring(2), 16).byteValue();
-	               break;
+//	@Override
+//	public Tuple getNext() throws IOException {
+//		Tuple input = super.getNext();
+//		// DefaultTuple result = new DefaultTuple();
+//		Tuple result = TupleFactory.getInstance().newTuple(3);
+//		
+//		// get rowkey
+//		result.set(0, new DataByteArray(input.get(0).toString()));
+//		String[] columns = input.get(1).toString().split(",");
+//		StringBuilder sb1 = new StringBuilder();
+//		StringBuilder sb2 = new StringBuilder();
+//		Map<String, DataByteArray> cfMap1 = new HashMap<String, DataByteArray>();
+//		Map<String, DataByteArray> cfMap2 = new HashMap<String, DataByteArray>();
+//
+//		for (int i = 0; i < columns.length; i++) {
+//			// if (i > 0) {
+//			// sb1.append("#,");
+//			// sb2.append("#,");
+//			// }
+//			// sb1.append(columns[i].subSequence(columns[i].indexOf('<'),
+//			// columns[i].indexOf('>') + 1));
+//			cfMap1.put(
+//					columns[i].subSequence(columns[i].indexOf('<'),
+//							columns[i].indexOf('>') + 1).toString(),
+//					new DataByteArray(""));
+//			// sb2.append(columns[i].subSequence(columns[i].lastIndexOf('<'),
+//			// columns[i].lastIndexOf('>') + 1));
+//			cfMap2.put(
+//					columns[i].subSequence(columns[i].lastIndexOf('<'),
+//							columns[i].lastIndexOf('>') + 1).toString(),
+//					new DataByteArray(""));
+//		}
+//
+//		// column key bag 1 und bag 2
+//		// result.append("[" + sb1.toString() + "]");
+//		result.set(1, cfMap1);
+//		// result.append("[" + sb2.toString() + "]");
+//		result.set(2, cfMap2);
+//
+//		// result.
+//		return result;
+//		// return super.getNext();
+//	}
 
-	            case 'u':
-	                this.fieldDel =
-	                    Integer.valueOf(delimiter.substring(2)).byteValue();
-	                break;
+	 @Override
+	public Tuple getNext() throws IOException {
+		try {
+			if (reader.nextKeyValue()) {
+				ImmutableBytesWritable rowKey = (ImmutableBytesWritable) reader
+						.getCurrentKey();
+				Result result = (Result) reader.getCurrentValue();
 
-	            default:
-	                throw new RuntimeException("Unknown delimiter " + delimiter);
-	            }
-	        } else {
-	            throw new RuntimeException("PigStorage delimeter must be a single character");
-	        }
-	    }
+				int tupleSize = 2;
 
-	    @Override
-	    public Tuple getNext() throws IOException {
-	        try {
-	            boolean notDone = in.nextKeyValue();
-	            if (notDone) {
-	                return null;
-	            }
-	            Text value = (Text) in.getCurrentValue();
-	            byte[] buf = value.getBytes();
-	            int len = value.getLength();
-	            int start = 0;
+				// use a map of families -> qualifiers with the most recent
+				// version of the cell. Fetching multiple vesions could be a
+				// useful feature.
+				NavigableMap<byte[], NavigableMap<byte[], byte[]>> resultsMap = result
+						.getNoVersionMap();
 
-	            for (int i = 0; i < len; i++) {
-	                if (buf[i] == fieldDel) {
-	                    readField(buf, start, i);
-	                    start = i + 1;
-	                }
-	            }
-	            // pick up the last field
-	            readField(buf, start, len);
+				if (true) {
+					tupleSize++;
+				}
+				Tuple tuple = TupleFactory.getInstance().newTuple(tupleSize);
 
-	            Tuple t =  mTupleFactory.newTupleNoCopy(mProtoTuple);
-	            mProtoTuple = null;
-	            return t;
-	        } catch (InterruptedException e) {
-	            int errCode = 6018;
-	            String errMsg = "Error while reading input";
-	            throw new ExecException(errMsg, errCode,
-	                    PigException.REMOTE_ENVIRONMENT, e);
-	        }
+				int startIndex = 0;
+				if (true) {
+					tuple.set(0, new DataByteArray(rowKey.get()));
+					startIndex++;
+				}
+				for (int i = 0; i < 2; ++i) {
+					int currentIndex = startIndex + i;
 
-	    }
+					if (true) {
+						// It's a column family so we need to iterate and set
+						// all
+						// values found
 
-	    private void readField(byte[] buf, int start, int end) {
-	        if (mProtoTuple == null) {
-	            mProtoTuple = new ArrayList<Object>();
-	        }
+						NavigableMap<byte[], byte[]> cfResults = resultsMap
+								.get("VALUE".getBytes());
+						Map<String, DataByteArray> cfMap = new HashMap<String, DataByteArray>();
 
-	        if (start == end) {
-	            // NULL value
-	            mProtoTuple.add(null);
-	        } else {
-	            mProtoTuple.add(new DataByteArray(buf, start, end));
-	        }
-	    }
+						if (cfResults != null) {
+							for (byte[] quantifier : cfResults.keySet()) {
+								// We need to check against the prefix filter to
+								// see if this value should be included. We
+								// can't
+								// just rely on the server-side filter, since a
+								// user could specify multiple CF filters for
+								// the
+								// same CF.
+								if (true) {
 
-	    @Override
-	    public InputFormat getInputFormat() {
-	        return new TextInputFormat();
-	    }
+									byte[] cell = cfResults.get(quantifier);
+									DataByteArray value = cell == null ? null
+											: new DataByteArray(cell);
+									cfMap.put(Bytes.toString(quantifier), value);
+								}
+							}
+						}
+						tuple.set(currentIndex, cfMap);
+					} else {
+						//
+					}
+				}
 
-	    @Override
-	    public void prepareToRead(RecordReader reader, PigSplit split) {
-	        in = reader;
-	    }
-
-	    @Override
-	    public void setLocation(String location, Job job)
-	            throws IOException {
-	        FileInputFormat.setInputPaths(job, location);
-	    }
-
-	// private static final String DELIM = "\t";
-	// private static final int DEFAULT_LIMIT = 226;
-	// private int limit = DEFAULT_LIMIT;
-	// private RecordReader reader;
-	// private List indexes;
-	// private TupleFactory tupleFactory;
-	//
-	// /**
-	// * Pig Loaders only take string parameters. The CTOR is really the only
-	// interaction
-	// * the user has with the Loader from the script.
-	// * @param indexesAsStrings
-	// */
-	// public PigLoadUDF(String...indexesAsStrings) {
-	// this.indexes = new ArrayList();
-	// for(String indexAsString : indexesAsStrings) {
-	// indexes.add(new Integer(indexAsString));
-	// }
-	//
-	// tupleFactory = TupleFactory.getInstance();
-	// }
-	//
-	//
-	// @Override
-	// public InputFormat getInputFormat() throws IOException {
-	// return new TextInputFormat();
-	//
-	// }
-	//
-	// /**
-	// * the input in this case is a TSV, so split it, make sure that the
-	// requested indexes are valid,
-	// */
-	// @Override
-	// public Tuple getNext() throws IOException {
-	// Tuple tuple = null;
-	// List values = new ArrayList();
-	//
-	// try {
-	// boolean notDone = reader.nextKeyValue();
-	// if (!notDone) {
-	// return null;
-	// }
-	// Text value = (Text) reader.getCurrentValue();
-	//
-	// if(value != null) {
-	// String parts[] = value.toString().split(DELIM);
-	//
-	// for(Integer index : indexes) {
-	// if(index > limit) {
-	// throw new IOException("index "+index+
-	// "is out of bounds: max index = "+limit);
-	// } else {
-	// values.add(parts[index]);
-	// }
-	// }
-	//
-	// tuple = tupleFactory.newTuple(values);
-	// }
-	//
-	// } catch (InterruptedException e) {
-	// // add more information to the runtime exception condition.
-	// int errCode = 6018;
-	// String errMsg = "Error while reading input";
-	// throw new ExecException(errMsg, errCode,
-	// PigException.REMOTE_ENVIRONMENT, e);
-	// }
-	//
-	// return tuple;
-	//
-	// }
-	//
-	// @Override
-	// public void prepareToRead(RecordReader reader, PigSplit pigSplit)
-	// throws IOException {
-	// this.reader = reader; // note that for this Loader, we don't care about
-	// the PigSplit.
-	// }
-	//
-	// @Override
-	// public void setLocation(String location, Job job) throws IOException {
-	// FileInputFormat.setInputPaths(job, location); // the location is assumed
-	// to be comma separated paths.
-	//
-	// }
-
+				return tuple;
+			}
+		} catch (InterruptedException e) {
+			throw new IOException(e);
+		}
+		return null;
+	}
 }
--- src/main/java/lupos/cloud/testing/Test.java
+++ src/main/java/lupos/cloud/testing/Test.java
@@ -18,23 +18,26 @@ public class Test {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-//		Properties props = new Properties();
-//		props.setProperty("fs.default.name", "hdfs://192.168.2.45:8020");
-//		props.setProperty("mapred.job.tracker", "192.168.2.45:8021");
-//		PigServer pigServer = new PigServer(ExecType.MAPREDUCE, props);
+		// Properties props = new Properties();
+		// props.setProperty("fs.default.name", "hdfs://192.168.2.45:8020");
+		// props.setProperty("mapred.job.tracker", "192.168.2.45:8021");
+		// PigServer pigServer = new PigServer(ExecType.MAPREDUCE, props);
 		PigServer pigServer = new PigServer(ExecType.LOCAL);
 		pigServer.registerFunction("PigLoadUDF", new FuncSpec(
 				"lupos.cloud.pig.udfs.PigLoadUDF"));
-		pigServer
-				.registerQuery("p1 = load 'hbase://S_PO' using org.apache.pig.backend.hadoop.hbase.HBaseStorage('VALUE', '-loadKey true') as (rowkey:bytearray, VALUE:map[]);");
-//		pigServer.registerQuery("data = load 'hbase://test_table' using org.apache.pig.backend.hadoop.hbase.HBaseStorage('stats:count', '-loadKey true') AS (id:chararray, count:chararray);");
-		Iterator i = pigServer.openIterator("p1");
+		 pigServer
+		 .registerQuery("p1 = load 'hbase://S_PO' using lupos.cloud.pig.udfs.PigLoadUDF('VALUE', '-loadKey true') as (rowkey:bytearray, pmap:map[], omap:map[]);");
+//		pigServer
+//				.registerQuery("p1 = load 'hbase://S_PO' using org.apache.pig.backend.hadoop.hbase.HBaseStorage('VALUE', '-loadKey true') as (rowkey:bytearray, pmap:map[], omap:map[]);");
+
+		// System.out.println(pigServer.getExamples("p1").toString());
+		Iterator<Tuple> i = pigServer.openIterator("p1");
 
 		while (i.hasNext()) {
 			// org.apache.pig.data.Tuple t = (org.apache.pig.data.Tuple)
 			i.next();
-			Tuple t = (Tuple) i.next();
-			System.out.println(t.get(1));
+			Tuple t = i.next();
+			System.out.println(t.toString());
 		}
 
 	}
